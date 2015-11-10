81ec15633ac5081ad110821617cd9f7b5e79e1ce
jinga50334 <abcd360@gmail.com>
2015-11-10 16:38:44 +0800

    branch 2015-11-10_16:38:44

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import subprocess
+subprocess.Popen('ls -a',shell=True)
+subprocess.Popen('echo hello',shell=True)
+subprocess.Popen('echo world',shell=True)
+subprocess.Popen('echo hufu',shell=True)
+subprocess.Popen('echo zhuzhu',shell=True)
+subprocess.Popen('echo pangdun',shell=True)
+subprocess.Popen('echo ruchao',shell=True)
